43d342a41e955393593a3026c373558b0c615d00
unknown <cadaciojerico@gmail.com>
2025-11-30 17:14:38 +0800

    Add create reservation service.

---
+++ app/services/reservations/create_reservation.rb
@@ -0,0 +1,34 @@
+module Reservations
+  class CreateReservation
+    PARSERS = {
+      format_1: Parsers::Format1,
+      format_2: Parsers::Format2
+    }.freeze
+
+    def self.call(payload)
+      new(payload).call
+    end
+
+    def initialize(payload)
+      @payload = payload
+    end
+
+    def call
+      format = PayloadIdentifier.call(@payload)
+      parser = PARSERS[format]
+
+      raise "Unsupported payload format: #{format}" if parser.nil?
+
+      parsed = parser.call(@payload)
+
+      guest = Guest.find_or_initialize_by(email: parsed[:guest][:email])
+      guest.assign_attributes(parsed[:guest])
+      guest.save!
+
+      reservation = guest.reservations.build(parsed[:reservation])
+      reservation.save!
+
+      reservation
+    end
+  end
+end
